ad65c66a53c497b55daa9894428011cb6477030f
Eric Le <mr.anh91@gmail.com>
2013-02-27 13:19:47 +0700

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,6 @@ VIP13::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
-  config.assets.debug = true
   config.assets.compile = true
   # Expands the lines which load the assets
   config.assets.debug = true
